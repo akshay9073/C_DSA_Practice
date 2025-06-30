1ebc2db92ba4e9df210312e174973a450271046a
akshay9073 <bhandari.akshay29@gmail.com>
2025-06-30 11:05:48 +0530

    Escape Sequence Code

---
+++ Problem_2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	printf("\n We are trying all the Escape Sequence in this Program");
+	
+	printf("\n1: Alert(\\a) \a -->This gives a beep sound");
+	printf("\n2: New Line(\\n) Reema \n Thereja ");
+	printf("\n3: Horizontal Tab(\\t) Reema \t Thereja ");
+	printf("\n4: Vertical Tab (\\v)  mostly not \v visible in all terminals");
+	printf("\n5: Backspace (\\b) ABC\bD -->deletes C and replace it with D");
+	printf("\n6: Carriage Return (\\r) Hello World \r this is C program -->This will return cursor to the start of line");
+	printf("\n7: Form Feed (\\f) This is \f line feed operator -->Behaves like page break ");
+	
+	return 0;
+}
