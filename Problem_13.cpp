d61297e35316333ee5bad9588be207c07a2c84f9
akshay9073 <bhandari.akshay29@gmail.com>
2025-07-02 22:02:30 +0530

    Addition of two floating numbers

---
+++ Problem_13.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	int result;
+	
+	printf("\nEnter any two numbers : ");
+	scanf("%f %f",&a,&b);
+	
+	result=a+b;
+	printf("Addition of two numbers : %d",result);
+	
+	return 0;
+}
