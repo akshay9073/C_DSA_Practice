135542f65630e59219d20b66cc325e81294b357a
akshay9073 <bhandari.akshay29@gmail.com>
2025-07-12 16:52:09 +0530

    Pattern 3

---
+++ Problem_56.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int rows,cols;
+	printf("\nEnter rows and cols : ");
+	scanf("%d %d",&rows,&cols);
+	
+	for(int i=1;i<=rows;i++)
+	{
+		for(int j=1;j<=cols;j++)
+		{
+			if(j%2==0)
+			printf("%d",1);
+			else
+			printf("%d",0);
+		}
+		printf("\n");
+	}
+}
