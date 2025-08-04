e6e523ef72e0bf0d2b16e596690135aa9ffaf680
akshay9073 <bhandari.akshay29@gmail.com>
2025-08-04 11:07:41 +0530

    modified strncpy function

--- Problem_139.c
+++ Problem_139.c
@@ -2,10 +2,13 @@
 #include <string.h>
 
 int main() {
-    char src[] = "Hello";
+    char src[20];
     char dest[20];
     
-    // Copies "Hello" to dest
+    printf("\nEnter String-1 : ");
+    scanf(" %[^\n]",&s1);
+    
+    printf("\nCopy the n-bytes from String\n");
     strncpy(dest, src, 4);
     printf("%s", dest);
     return 0;
