c10c82f1ecc3f8e14361934aef5cb23965e27f0c
akshay9073 <bhandari.akshay29@gmail.com>
2025-08-13 23:54:53 +0530

    Bitwise | operation

---
+++ Problem_167.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("\nEnter any two integers : ");
+	scanf("%d %d",&a,&b);
+	
+	printf("\nBitwise OR operations : ");
+	printf("\t a | b = %d",a | b);
+	
+	return 0;
+}
