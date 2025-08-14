f6d36d80909a885f68f70a41d4eb7cc8e7f223b1
akshay9073 <bhandari.akshay29@gmail.com>
2025-08-14 23:54:40 +0530

    Accept_Sparse_Matrix() in Sparse matrix code

--- Problem_170.c
+++ Problem_170.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 void accept_matrix(int r,int c,int [][c]);
 void display_matrix(int r,int c,int [][c]);
+void accept_sm(int smat[][3],int);
 int main()
 {
 	int mat[20][20],r,c;
-	
+	int smat[20][3];
 	int choice;
 	
 	
@@ -13,6 +14,7 @@ int main()
 		printf("\n==========================================\n");
 		printf("\n 1)Accept Matrix");
 		printf("\n 2)Display Matrix");
+		printf("\n 3)Accept Sparse Matrix");
 		printf("\n===========================================\n");
 		
 		printf("\nEnter the choice : ");
@@ -29,6 +31,12 @@ int main()
 			case 2:
 				printf("\nDisplay Matrix\n");
 				display_matrix(r,c,mat);
+				break;
+				
+			case 3:
+				printf("\nEnter the rows you want in sparse matrix : ");
+				scanf("%d",&r);
+				accept_sm(smat,r);
 				break;
 				
 				
@@ -56,5 +64,16 @@ void display_matrix(int r,int c,int mat[][c])
 		printf("%d\t",mat[i][j]);
 		
 		printf("\n");
+	}
+}
+
+void accept_sm(int smat[][3],int r)
+{
+	printf("\nEnter data in sparse matrix : ");
+	
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<3;j++)
+		scanf("%d",&smat[i][j]);
 	}
 }
