4f06c585944ed311ed92e9c614bd3024a978e94f
akshay9073 <bhandari.akshay29@gmail.com>
2025-08-18 15:05:10 +0530

    Added create and display linked list function

--- Problem_174.c
+++ Problem_174.c
@@ -1,28 +1,94 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 typedef struct node
 {
 	int data;
 	struct node *next;
 }node;
-
+node *create();
+node *cn();
+void display(node *);
 int main()
 {
 	int choice;
+	node *HEAD=NULL;
 	
-	printf("\n===========================================================\n");
-	printf("\n 1)Create a SLL");
-	printf("\n 2)Display a SLL");
-	printf("\n===========================================================\n");
+	while(1)
+	{
+		printf("\n===========================================================\n");
+		printf("\n 1)Create a SLL");
+		printf("\n 2)Display a SLL");
+		printf("\n===========================================================\n");
 	
 	printf("\nEnter your choice : ");
 	scanf("%d",&choice);
 	
-	
+	switch(choice)
+	{
+		case 1:
+			HEAD=create();
+			printf("\nSingly Linked List create Success!!");
+			break;
+			
+		case 2:
+			display(HEAD);
+			break;
+			
+	}
+	}
 	
 }
 
 node *cn()
 {
 	return (node *)malloc(sizeof(node));
+}
+
+node *create()
+{
+	int x,n;
+	node *head=NULL,*p,*q;
+	
+	printf("\nEnter how many nodes you want? : ");
+	scanf("%d",&n);
+	
+	head=cn();
+	printf("\nEnter the data : ");
+	scanf("%d",&x);
+	head->data=x;
+	head->next=NULL;
+	
+	p=head;
+	int i=1;
+	while(i<n)
+	{
+		q=cn();
+		printf("\nEnter the data : ");
+		scanf("%d",&x);
+		q->data=x;
+		q->next=NULL;
+		p->next=q;
+		
+		p=p->next;
+		i++;
+	}
+	
+	return head;
+}
+
+void display(node *temp)
+{
+	if(temp==NULL)
+	{
+		printf("\nSSL is empty!!");
+	}
+	else
+	{
+		while(temp!=NULL)
+		{
+			printf("%d\t",temp->data);
+			temp=temp->next;
+		}
+	}
 }
