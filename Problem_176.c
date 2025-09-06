a8bb45866b4ff17ea59cbeae848cda754417b106
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-06 14:12:45 +0530

    Display function added

--- Problem_176.c
+++ Problem_176.c
@@ -8,6 +8,8 @@ typedef struct node
 }node;
 
 node *cn();
+node *create();
+void display(node *);
 int main()
 {
     node *HEAD=NULL; 
@@ -16,11 +18,26 @@ int main()
     while(1)
     {
         printf("\n=========Linked List==========\n");
-		
+		printf("\n1)Create Linked List");
+		printf("\n2)Display");
         printf("\n==============================\n");
 
         printf("\nEnter Your Choice : ");
         scanf("%d",&ch);
+        
+        switch(ch)
+        {
+        	case 1:
+                HEAD = create();
+                printf("\nDLL Created Success!!!");
+                break;
+                
+            case 2:
+            	printf("\nDisplay Linked List : ");
+            	display(HEAD);
+            	break;
+
+		}
 	}
 	
 	
@@ -29,4 +46,51 @@ node* cn()
 {
     return (node*)malloc(sizeof(node));
 }
+
+node *create()
+{
+    int n , x , i = 1 ;
+    node *head=NULL ,*p ,*q;
+    printf("\nHow Many Nodes U Want : ");
+    scanf("%d",&n); 
+
+    head = cn();
+    printf("\nEnter Data : ");
+    scanf("%d",&x);
+    head -> prev = NULL;
+    head -> data = x;
+    head -> next = NULL;
+
+    p = head;
+
+    while(i<n)
+    {
+        q = cn();
+        printf("\nEnter Data : ");
+        scanf("%d",&x);
+
+        q->prev = p;
+        q -> data = x;
+        q -> next = NULL;
+
+        p->next = q ;
+        p = p->next; 
+        i++;
+    }
+    return head;
+}
+
+void display(node *head)
+{
+	node *p=head;
+	
+	while(p!=NULL)
+	{
+		printf("\t%d",p->data);
+		p=p->next;
+	}
+	
+	
+}
+
 
