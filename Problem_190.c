b37bc36ee2158cd71f2b60ad2f79f0bed5c1f424
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-26 16:48:57 +0530

    infix to postfix added

--- Problem_190.c
+++ Problem_190.c
@@ -1,14 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 #define MAX 100
 
 typedef struct stack{
 	char data[MAX];
 	int top;
 }stack;
+void init(stack *);
+int is_empty(stack *);
+int is_full(stack *);
+void push(stack *,char);
+char pop(stack *);
+char top(stack *);
+int priority(char);
+int main()
+{
+	stack s;
+	init(&s);
+	
+	char exp[20];
+	
+	printf("\nEnter the expression : ");
+	scanf(" %s",exp);
+	
+	int i=0;
+	char x;
+	while(exp[i]!='\0')
+	{
+		if(isalnum(exp[i]))
+		printf("%c",exp[i]);
+		else if(exp[i]=='(')
+		push(&s,exp[i]);
+		else
+		{
+			if(exp[i]==')')
+			{
+				while((x=pop(&s))!='(')
+				printf("%c",x);
+			}
+			else
+			{
+				while(!is_empty(&s) && priority(exp[i])<=priority(top(&s)))
+				{
+					printf("%c",pop(&s));
+				}
+				push(&s,exp[i]);
+			}
+		}
+		i++;
+	}
+	
+	while(!is_empty(&s))
+	{
+		printf("%c",pop(&s));
+	}
+	
+}
 
-int init(stack *s)
+
+
+
+
+
+
+
+
+
+
+
+
+void init(stack *s)
 {
 	s->top=-1;
 }
@@ -31,7 +93,7 @@ int is_empty(stack *s)
 
 void push(stack *s, char x)
 {
-	if(is_full(&s))
+	if(is_full(s))
 	{
 		printf("\nStack is full !!");
 		exit(0);
@@ -46,7 +108,7 @@ void push(stack *s, char x)
 char pop(stack *s)
 {
 	char x;
-	if(is_empty())
+	if(is_empty(&s))
 	{
 		printf("\nStack is empty!!");
 		exit(0);
@@ -64,4 +126,14 @@ char top(stack *s)
 	return s->data[s->top];
 }
 
-
+int priority(char x)
+{
+	if(x=='(')
+	return 0;
+	else if(x=='+'|| x=='-')
+	return 1;
+	else if(x=='*'||x=='/'||x=='%')
+	return 2;
+	else
+	return 3;
+}
