90ce519b0e35c79894775580666f4ec4f1685768
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-27 00:41:35 +0530

    infix to postfix using stack (linked-list)

--- Problem_192.c
+++ Problem_192.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
-
+#include<stdlib.h>
 
 typedef struct node{
 	char data;
@@ -14,21 +14,49 @@ int  push(char);
 char pop();
 int is_empty();
 int priority(char);
-
+char top_element();
 node *cn()
 {
-	return (node *)malloc(sizeof(node*));
+	return (node *)malloc(sizeof(node));
 }
 
 int main()
 {
-		push('a');
-		push('k');
-		push('s');
+		char exp[20];
+		printf("\nEnter the expression : ");
+		scanf("%s",exp);
 		
-		printf("%c\t",pop());
-		printf("%c\t",pop());
-		printf("%c\t",pop());
+		int i=0;
+		char x;
+		while(exp[i]!='\0')
+		{
+			if(isalnum(exp[i]))
+			printf("%c",exp[i]);
+			else if(exp[i]=='(')
+			push(exp[i]);
+			else
+			{
+				if(exp[i]==')')
+				{
+					while(x=pop()!='(')
+					printf("%c",x);
+				}
+				else
+				{
+					while(!is_empty() && (priority(exp[i])<=priority(top_element())))
+					{
+						printf("%c",pop());
+					}
+					push(exp[i]);
+				}
+			}
+			i++;
+		}
+		
+		while(!is_empty())
+		{
+			printf("%c",pop());
+		}
 }
 
 int is_empty()
@@ -74,16 +102,20 @@ char pop()
 	}
 }
 
-int piority(char x)
+int priority(char x)
 {
 	if(x=='(')
 	return 0;
-	else if(x=='+' || x=='-');
+	else if(x=='+'|| x=='-')
 	return 1;
-	else if(x=='*' || x=='/' || x=='%')
+	else if(x=='*'||x=='/'||x=='%')
 	return 2;
-	else 
+	else
 	return 3;
 }
 
+char top_element()
+{
+	return top->data;
+}
 
