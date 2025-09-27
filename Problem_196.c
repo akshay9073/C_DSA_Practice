46dfccecc18d468be8bd262e2ddc9bee04c0b2ae
akshay_9073 <bhandari.akshay29@gmail.com>
2025-09-28 00:52:30 +0530

    infix to prefix function

--- Problem_196.c
+++ Problem_196.c
@@ -10,26 +10,90 @@ typedef struct stack{
 	int top;
 }stack;
 
-int top(stack *);
-int pop(stack *);
-void push(stack *,int);
+char top(stack *);
+char pop(stack *);
+void push(stack *,char);
 int is_empty(stack *);
 int is_full(stack *);
-int init(stack *);
-
+void init(stack *);
+int priority(char);
 int main()
 {
+	stack s;
+	init(&s);
+	char exp[20],infix[20],prefix[20];
 	
+	printf("\nEnter the expression : ");
+	scanf("%s",exp);
+	
+	strcpy(infix,exp);
+	
+	strrev(exp);
+	
+	for(int i=0;exp[i]!='\0';i++)
+	{
+		if(exp[i]=='(')
+		exp[i]=')';
+		else if(exp[i]==')')
+		exp[i]='(';
+	}
+	
+	printf("\nExpression is : %s",exp);
+	
+	
+	int i=0,k=0;
+	char x;
+	while(exp[i]!='\0')
+	{
+		if(isalnum(exp[i]))
+		prefix[k++]=exp[i];
+		else if(exp[i]=='(')
+			push(&s,exp[i]);
+		else
+		{
+			if(exp[i]==')')
+			{
+				while((x=pop(&s))!='(')
+				prefix[k++]=x;
+			}
+			else
+			{
+				while(!is_empty(&s) && priority(exp[i])<=priority(top(&s)))
+				{
+					prefix[k++]=pop(&s);
+				}
+				push(&s,exp[i]);
+			}	
+		}
+		i++;	
+	}
+	
+	while(!is_empty(&s))
+	prefix[k++]=pop(&s);
+	
+	prefix[k]='\0';
+	strrev(prefix);
+	printf("\nInfix expression : %s",infix);
+	printf("\nPrefix expression : %s",prefix);
+	
+	return 0;
+}
+	
+
+
+int priority(char x)
+{
+	if(x=='(')
+	return 0;
+	else if(x=='+' || x=='-')
+	return 1;
+	else if(x=='*' || x=='/' || x=='%')
+	return 2;
+	else
+	return 3;
 }
 
-
-
-
-
-
-
-
-void push(stack *s,int x)
+void push(stack *s,char x)
 {
 	if(is_full(s))
 	{
@@ -43,7 +107,7 @@ void push(stack *s,int x)
 	}
 }
 
-int pop(stack *s)
+char pop(stack *s)
 {
 	if(is_empty(s))
 	{
@@ -52,13 +116,13 @@ int pop(stack *s)
 	}
 	else
 	{
-		int x=s->data[s->top];
+		char x=s->data[s->top];
 		s->top-=1;
 		return x;
 	}
 }
 
-int top(stack *s)
+char top(stack *s)
 {
 	return s->data[s->top];
 }
@@ -75,10 +139,11 @@ int is_empty(stack *s)
 {
 	if(s->top==-1)
 	return 1;
-	else 0;
+	else 
+	return 0;
 }
 
-int init(stack *s)
+void init(stack *s)
 {
 	s->top=-1;
 }
