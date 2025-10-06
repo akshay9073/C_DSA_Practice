d2b470e264f163eb9f002edbf5478b0c402d0d4c
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-06 14:09:40 +0530

    Queue menu driven program basic program structure

---
+++ Problem_206.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+#deinfe MAX 100
+
+typedef struct queue{
+	int data[MAX];
+	int f,q;
+}queue;
+
+int main()
+{
+	int choice;
+	
+	while(1)
+	{
+		printf("\n=============================================\n");
+		printf("1)")
+		printf("\n=============================================\n");	
+		
+		printf("\nEnter your Choice : ");
+		scanf("%d",&choice);
+		
+		swithc(choice)
+		{
+			
+			
+		}
+	}	
+	
+}
+
+
