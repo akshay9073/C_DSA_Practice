f3d3042cd10568578dade6d44a21b93a42a43417
akshay_9073 <bhandari.akshay29@gmail.com>
2025-10-18 18:22:43 +0530

    delete exe .bat file added

---
+++ Delete_exe.bat
@@ -0,0 +1,9 @@
+@echo off
+REM ======================================
+REM Delete all .exe files in the current folder
+REM ======================================
+
+del /q *.exe
+
+echo All .exe files in this folder have been deleted.
+pause
